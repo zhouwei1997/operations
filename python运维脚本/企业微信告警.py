314bec857f88e0e0f625172e2632399c4796578a
zhouwei1997 <hengyu19971023@163.com>
2022-03-17 21:46:38 +0800

    企业微信告警

---
+++ python运维脚本/企业微信告警.py
@@ -0,0 +1,28 @@
+# 企业微信告警
+# 此脚本通过企业微信应用，进行微信告警，可用于Zabbix监控
+import requests
+
+
+class DLF:
+    def __init__(self,corpid,corpsecret):
+        self.url = 'https://qyapi.weixin.qq.com/cgi-bin'
+        self.corpid = corpid
+        self.corpsecret = corpsecret
+        self._token=self._get_token()
+
+    def __get_token(self):
+       '''
+       获取企业微信的API接口的access_token
+       :return:
+       '''
+       token_url = self.url + "/gettoken?corpid=%s&corpsecret=%s" %(self.corpid, self.corpsecret)
+       try:
+           res=requests.get(token_url).json()
+           token=res['access_token']
+           return token
+       except Exception as e:
+           return str(e)
+
+    def __get_media_id(self):
+        get_media_url=self.url + "/media/upload?access_token={}&type=file".format(self._token)
+        #data={"media":file_obj}
