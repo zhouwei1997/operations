544ff7a46afa0512e0102f7289ccb8e8d7e45e30
zhouwei1997 <hengyu19971023@163.com>
2022-03-18 09:36:44 +0800

    企业微信告警

--- python运维脚本/企业微信告警.py
+++ python运维脚本/企业微信告警.py
@@ -6,27 +6,27 @@ import requests
 
 
 class DLF:
-    def __init__(self,corpid,corpsecret):
+    def __init__(self, corpid, corpsecret):
         self.url = 'https://qyapi.weixin.qq.com/cgi-bin'
         self.corpid = corpid
         self.corpsecret = corpsecret
-        self._token=self._get_token()
+        self._token = self._get_token()
 
     def __get_token(self):
-       '''
-       获取企业微信的API接口的access_token
-       :return:
-       '''
-       token_url = self.url + "/gettoken?corpid=%s&corpsecret=%s" %(self.corpid, self.corpsecret)
-       try:
-           res=requests.get(token_url).json()
-           token=res['access_token']
-           return token
-       except Exception as e:
-           return str(e)
+        '''
+        获取企业微信的API接口的access_token
+        :return:
+        '''
+        token_url = self.url + "/gettoken?corpid=%s&corpsecret=%s" % (self.corpid, self.corpsecret)
+        try:
+            res = requests.get(token_url).json()
+            token = res['access_token']
+            return token
+        except Exception as e:
+            return str(e)
 
     def __get_media_id(self, file_obj):
-        get_media_url=self.url + "/media/upload?access_token={}&type=file".format(self._token)
+        get_media_url = self.url + "/media/upload?access_token={}&type=file".format(self._token)
         data = {"media": file_obj}
         try:
             res = requests.post(url=get_media_url, files=data)
