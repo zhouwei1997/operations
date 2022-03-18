afaca99c19ed538900eb7d2d4d0742290df51651
zhouwei1997 <hengyu19971023@163.com>
2022-03-18 12:25:36 +0800

    SSH客户端

--- python运维脚本/SSH客户端.py
+++ python运维脚本/SSH客户端.py
@@ -6,27 +6,27 @@ import paramiko as paramiko
 
 class SSHClient:
     def __init__(self, host, port, user, pkey):
-        self.ssh_host=host
-        self.ssh_port=port
-        self.ssh_user=user
-        self.private_key=paramiko.RSAKey.from_private_key_file(pkey)
-        self.ssh=None
+        self.ssh_host = host
+        self.ssh_port = port
+        self.ssh_user = user
+        self.private_key = paramiko.RSAKey.from_private_key_file(pkey)
+        self.ssh = None
         self._connect()
 
     def _connect(self):
-        self.ssh=paramiko.SSHClient()
+        self.ssh = paramiko.SSHClient()
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         try:
-            self.ssh.connect(hostname=self.ssh_host,port=self.ssh_port,username=self.ssh_user,pkey=self.private_key,
+            self.ssh.connect(hostname=self.ssh_host, port=self.ssh_port, username=self.ssh_user, pkey=self.private_key,
                              timeout=10)
         except:
             return 'ssh connect fail'
 
-    def execute_command(self,command):
-        stdin,stdout,stderr=self.ssh.exec_command(command)
-        out=stdout.read()
-        err=stderr.read()
-        return out,err
+    def execute_command(self, command):
+        stdin, stdout, stderr = self.ssh.exec_command(command)
+        out = stdout.read()
+        err = stderr.read()
+        return out, err
 
     def close(self):
         self.ssh.close()
---
+++ python运维脚本/Saltstack客户端.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# author: zhouwei
+# description: 通过 api 对 Saltstack 服务端进行操作，执行命令。
+
+class saltApi:
+    '''
+    定义salt api接口的类
+    初始化获得token
+    '''
+    def __init__(self):
+        self.url="htp:"
